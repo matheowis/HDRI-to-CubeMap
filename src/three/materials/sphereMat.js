4979e7ed825e151e0ff8e0ecea03fc5d7260e496
Wiśniowski Mateusz <mateusz.wisniowski@wisniowski.pl>
2018-07-26 15:09:23 +0200

    preview added

--- src/three/materials/sphereMat.js
+++ src/three/materials/sphereMat.js
@@ -1,10 +1,26 @@
-import { MeshBasicMaterial, DoubleSide, ShaderMaterial, ShaderChunk, UniformsUtils, UniformsLib } from 'three';
-
+import { MeshBasicMaterial, DoubleSide,RGBEEncoding,NearestFilter, ShaderMaterial, ShaderChunk, UniformsUtils, UniformsLib,TextureLoader } from 'three';
+import {RGBELoader} from '../examples/RGBELoader'
 
 
 const sphereMat = new MeshBasicMaterial({ color: 0xffffff, map: null, side: DoubleSide });
 
+const loader = new RGBELoader()
 
+loader.load(
+  'images/venetian_crossroads_1k.hdr',
+  tex =>{
+
+    tex.encoding = RGBEEncoding;
+    tex.minFilter = NearestFilter;
+    tex.magFilter = NearestFilter;
+    tex.flipY = true;
+
+    sphereMat.map = tex;
+    sphereMat.needsUpdate = true;
+
+
+  }
+)
 
 const updateSphereMap = (map) => {
 
