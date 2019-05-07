aad8bb878bc1ffc2381bf85a06021f2b10c8d6ef
Wiśniowski Mateusz <mateuszwis93@gmail.com>
2019-05-07 08:55:53 +0200

    used encoding chunk

--- src/three/shaders/fragmentRgbm16.glsl
+++ src/three/shaders/fragmentRgbm16.glsl
@@ -21,44 +21,57 @@ varying vec2 vUv;
 // }
 
 ///http://marcinignac.com/blog/pragmatic-pbr-hdr
-vec3 rgbe2rgb(vec4 rgbe) {
-  return (rgbe.rgb * pow(2.0, rgbe.a * 255.0 - 128.0));
-}
+// vec3 rgbe2rgb(vec4 rgbe) {
+//   return (rgbe.rgb * pow(2.0, rgbe.a * 255.0 - 128.0));
+// }
 ///http://marcinignac.com/blog/pragmatic-pbr-hdr
-vec3 tonemapReinhard(vec3 color) {
-  return color / (color + vec3(1.0));
-}
+// vec3 tonemapReinhard(vec3 color) {
+//   return color / (color + vec3(1.0));
+// }
 
 /// https://github.com/glslify/glsl-gamma/blob/master/out.glsl
-const float gamma = 2.2;
-vec3 toGamma(vec3 v) {
-  return pow(v, vec3(1.0 / gamma));
-}
+// const float gamma = 2.2;
+// vec3 toGamma(vec3 v) {
+//   return pow(v, vec3(1.0 / gamma));
+// }
 
 /// http://graphicrants.blogspot.com/2009/04/rgbm-color-encoding.html
-vec4 RGBMEncode(vec3 color){
-  vec4 rgbm;
-  color *= 1.0 / 6.0;
-  rgbm.w = clamp( max( max( color.r, color.g ), max( color.b, 1e-6 ) ),0.0,1.0 );
-  rgbm.w = ceil( rgbm.w * 255.0 ) / 255.0;
-  rgbm.xyz = color / rgbm.w;
-  return rgbm;
-}
+// vec4 RGBMEncode(vec3 color){
+//   vec4 rgbm;
+//   color *= 1.0 / 6.0;
+//   rgbm.w = clamp( max( max( color.r, color.g ), max( color.b, 1e-6 ) ),0.0,1.0 );
+//   rgbm.w = ceil( rgbm.w * 255.0 ) / 255.0;
+//   rgbm.xyz = color / rgbm.w;
+//   return rgbm;
+// }
+
+// reference: http://iwasbeingirony.blogspot.ca/2010/06/difference-between-rgbm-and-rgbd.html
+// it is already defined
+// vec4 RGBMToLinear2( in vec4 value, in float maxRange ) {
+// 	return vec4( value.rgb * value.a * maxRange, 1.0 );
+// }
+
+// vec4 LinearToRGBM2( in vec4 value, in float maxRange ) {
+// 	float maxRGB = max( value.r, max( value.g, value.b ) );
+// 	float M = clamp( maxRGB / maxRange, 0.0, 1.0 );
+// 	M = ceil( M * 255.0 ) / 255.0;
+// 	return vec4( value.rgb / ( M * maxRange ), M );
+// }
+
+// most functions https://github.com/mrdoob/three.js/blob/dev/src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl.js
 
 void main() {
-  float uExposure = 1.0;
 
   vec4 texelColor = texture2D( tDiffuse, vUv );
 
-  texelColor.rgb *= uExposure;
+  texelColor = RGBEToLinear(texelColor);
 
-  texelColor.rgb = rgbe2rgb(texelColor);
+  texelColor = LinearToGamma(texelColor, 2.2);
 
-  texelColor.rgb = toGamma(texelColor.rgb);
+  texelColor = LinearToRGBM(texelColor, 6.0);
 
-  // texelColor.rgb = tonemapReinhard(texelColor.rgb);
+  // color test
+  // texelColor = RGBMToLinear(texelColor, 6.0);
 
-  vec4 rgbm = RGBMEncode(texelColor.rgb);
-
-  gl_FragColor = rgbm;
+  gl_FragColor = texelColor;
 }
